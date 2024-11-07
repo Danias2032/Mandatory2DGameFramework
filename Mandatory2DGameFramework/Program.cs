b8280b69e74eacce9520d01bf950576872d75449
Danias2036 <Dani67h0@edu.zealand.dk>
2024-11-07 11:45:43 +0100

    Add project files.

---
+++ Mandatory2DGameFramework/Program.cs
@@ -0,0 +1,37 @@
+﻿using Mandatory2DGameFramework.model.attack;
+using Mandatory2DGameFramework.model.Cretures;
+using Mandatory2DGameFramework.model.defence;
+using Mandatory2DGameFramework.worlds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            World world = new World(10, 10);
+
+            AttackItem sword = new AttackItem { Name = "Sword", Hit = 15, Range = 1};
+            DefenceItem shield = new DefenceItem { Name = "Shield", ReduceHitPoint = 5 };
+
+            Creature creature1 = new Creature("Warrior", 2, 3, 100, sword, null);
+            Creature creature2 = new Creature("Warrior", 2, 3, 100, null, shield);
+            Creature creature3 = new Creature("Warrior", 2, 3, 100, sword, shield);
+
+            world.AddCreature(creature1);
+            world.AddCreature(creature2);
+            world.AddCreature(creature3);
+
+            creature1.Hit(creature2);
+            creature2.Hit(creature3);
+            creature3.Hit(creature1);
+
+            Console.WriteLine($"{creature2.Name}'s remaining HP: {creature2.HitPoints}");
+        }
+    }
+}
---
+++ Mandatory2DGameFramework/model/Creatures/Creature.cs
@@ -0,0 +1,88 @@
+﻿using Mandatory2DGameFramework.model.attack;
+using Mandatory2DGameFramework.model.defence;
+using Mandatory2DGameFramework.worlds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.model.Cretures
+{
+    public class Creature
+    {
+        public string Name { get; set; }
+        public int HitPoints { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+        // Todo consider how many attack / defence weapons are allowed
+        public AttackItem? Attack { get; set; }
+        public DefenceItem? Defence { get; set; }
+
+        public Creature(string name, int hitPoints, int x, int y, AttackItem? attack = null, DefenceItem? defence = null)
+        {
+            Name = name;
+            HitPoints = 100;
+            X = y;
+            Y = x;
+            HitPoints = hitPoints;
+            Attack = attack;
+            Defence = defence;
+
+        }
+
+        public int Hit(Creature target)
+        {
+            if (Attack != null)
+            {
+                int damage = Attack.Hit;
+                target.ReceiveHit(damage);
+                Console.WriteLine($"{Name} hits {target.Name} with {Attack.Name} for {damage} damage");
+                return damage;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} has no weapon");
+                return 0;
+            }
+        }
+
+        public void ReceiveHit(int hit)
+        {
+            int defensePoints = Defence?.ReduceHitPoint ?? 0;
+            int damage = Math.Max(0, hit - defensePoints);
+            HitPoints -= damage;
+            Console.WriteLine($"{Name} receives {damage} damage, remaining HP: {HitPoints}");
+
+            if (HitPoints <= 0)
+            {
+                Console.WriteLine($"{Name} has been defeated");
+            }
+        }
+
+        public void Loot(WorldObject obj)
+        {
+            if (obj is AttackItem attackItem)
+            {
+                Attack = attackItem;
+                Console.WriteLine($"{Name} loots {attackItem.Name} as a weapon.");
+            }
+            else if (obj is DefenceItem defenceItem)
+            {
+                Defence = defenceItem;
+                Console.WriteLine($"{Name} loots {defenceItem} as armor.");
+            }
+            else 
+            {
+                Console.WriteLine($"{Name} cannot loot this item.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Name)}={Name}, {nameof(HitPoints)}={HitPoints}, {nameof(Attack)}={Attack}, {nameof(Defence)}={Defence}}}";
+        }
+    }
+}
